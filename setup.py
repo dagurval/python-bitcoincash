3b6a814c7d21a2a9608530ea18890bec2947112a
Dagur Valberg Johannsson <dagurval@pvv.ntnu.no>
2019-11-21 09:26:23 +0000

    Initial release

--- setup.py
+++ setup.py
@@ -11,13 +11,13 @@ with open(os.path.join(here, 'README.md')) as f:
 
 requires = []
 
-setup(name='python-bitcoincashlib',
+setup(name='bitcoincash',
       version=__version__,
       description='The Swiss Army Knife of the Bitcoin Cash protocol.',
       long_description=README,
       long_description_content_type='text/markdown',
       classifiers=[
-          "Programming Language :: Python",
+          "Programming Language :: Python :: 3",
           "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
       ],
       url='https://github.com/dagurval/python-bitcoincashlib',
